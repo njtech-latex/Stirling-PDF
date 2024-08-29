26b99f9cd13f104853100123087f7c4821f801c8
Cael <MR-Addict@qq.com>
2024-08-29 16:43:00 +0800

    feat: change default lang to zh_CN and add iframe script

---
+++ src/main/resources/static/js/iframe.js
@@ -0,0 +1,15 @@
+(() => {
+  // post message to parent window
+  const postmessage = (message) => window.parent.postMessage({ stirlingPDF: message }, "*");
+
+  // listen window width and height
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      const { width, height } = entry.contentRect;
+      postmessage({ width, height });
+    }
+  });
+
+  // observe body element
+  resizeObserver.observe(document.body);
+})();
